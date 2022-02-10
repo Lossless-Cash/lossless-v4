ce79466861b52e33f63265c4ddfc0699343b837a
Ignacio Freire <nachofreire@gmail.com>
2022-02-10 10:10:49 -0300

    Added test for committee claim

---
+++ contracts/forge_tests/environment.t.sol
@@ -0,0 +1,70 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+import "./utils/LosslessEnv.t.sol";
+
+contract EnvironmentTests is LosslessTestEnvironment {
+
+    /// @notice Test deployed Controller variables
+    function testControllerDeploy() public {
+      assertEq(lssController.getVersion() , 4);
+      assertEq(lssController.admin(), address(this));
+      assertEq(lssController.pauseAdmin(), address(this));
+      assertEq(lssController.recoveryAdmin(), address(this));
+    }
+
+    /// @notice Test deployed Reporting Config
+    function testReportingDeploy() public {
+      assertEq(lssReporting.reportLifetime(), reportLifetime);
+      assertEq(lssReporting.reportingAmount(), reportingAmount);
+      assertEq(address(lssReporting.losslessController()), address(lssController));
+      assertEq(address(lssReporting.stakingToken()), address(lssToken));
+      assertEq(address(lssReporting.losslessGovernance()), address(lssGovernance));
+
+      (uint256 configReporterReward, uint256 configLosslessReward, uint256 configCommitteeReward, uint256 configStakerReward) = lssReporting.getRewards();
+
+      assertEq(configReporterReward, reporterReward);
+      assertEq(configLosslessReward, losslessReward);
+      assertEq(configCommitteeReward, committeeReward);
+      assertEq(configStakerReward, stakersReward);
+    }
+
+    /// @notice Test deployed Staking Contract
+    function testStakingDeploy() public {
+      assertEq(lssStaking.stakingAmount(), stakingAmount);
+      assertEq(address(lssStaking.stakingToken()), address(lssToken));
+      assertEq(address(lssStaking.losslessReporting()), address(lssReporting));
+      assertEq(address(lssStaking.losslessGovernance()), address(lssGovernance));
+      assertEq(address(lssStaking.losslessController()), address(lssController));
+    }
+
+    /// @notice Test deployed Governance Contract
+    function testGovernanceDeploy() public {
+      assertEq(lssGovernance.walletDisputePeriod(), walletDispute);
+      assertEq(address(lssGovernance.losslessStaking()), address(lssStaking));
+      assertEq(address(lssGovernance.losslessReporting()), address(lssReporting));
+      assertEq(address(lssGovernance.losslessController()), address(lssController));
+
+      for (uint8 i = 0; i < committeeMembers.length; i++) {
+        assertTrue(lssGovernance.isCommitteeMember(committeeMembers[i]));
+      }
+    }
+
+    /// @notice Test deployed LssToken
+    function testLssTokenDeploy() public {
+      assertEq(lssToken.totalSupply(), totalSupply);
+      assertEq(lssToken.name(), "Lossless Token");
+      assertEq(lssToken.admin(), address(this));
+      assertEq(lssToken.recoveryAdmin(), address(this));
+      assertEq(lssToken.timelockPeriod(), 1 days);
+    }
+
+    /// @notice Test deployed Random LERC20 Token
+    function testLERC20TokenDeploy() public {
+      assertEq(lerc20Token.totalSupply(), totalSupply);
+      assertEq(lerc20Token.name(), "Random LERC20 Token");
+      assertEq(lerc20Token.admin(), randTokenAdmin);
+      assertEq(lerc20Token.recoveryAdmin(), randTokenAdmin);
+      assertEq(lerc20Token.timelockPeriod(), 1 days);
+    }
+}
--- contracts/forge_tests/rewards.t.sol
+++ contracts/forge_tests/rewards.t.sol
@@ -4,4 +4,55 @@ pragma solidity ^0.8.0;
 import "./utils/LosslessEnv.t.sol";
 
 contract RewardsTests is LosslessTestEnvironment {
+
+  /// @notice Test Committee members claiming their rewards when all participating
+  /// @dev Should not revert and update balances correctly
+  ///      reported amount * committee rewards / all members
+  function testMembersClaimAllParticipating() public {
+    uint256[5] memory memberBalances;
+
+    for (uint i = 0; i < committeeMembers.length; i++) {
+      memberBalances[i] = lerc20Token.balanceOf(committeeMembers[i]);
+    }
+
+    uint256 reportId = generateReport(address(lerc20Token), maliciousActor, reporter);
+    solveReportPositively(reportId);
+
+    for (uint i = 0; i < committeeMembers.length; i++) {
+      evm.prank(committeeMembers[i]);
+      lssGovernance.claimCommitteeReward(reportId);
+      uint256 newBalance =  memberBalances[i] + ((reportedAmount * committeeReward) / 1e2) / committeeMembers.length;
+      assertEq(lerc20Token.balanceOf(committeeMembers[i]), newBalance);  
+    }
+  }
+
+  /// @notice Test Committee members claiming their rewards when some participating
+  /// @dev Should not revert and update balances correctly
+  ///      reported amount * committee rewards / all members
+  function testMembersClaimSomeParticipating() public {
+    uint totalMembers = committeeMembers.length;
+    uint256[5] memory memberBalances;
+    uint256 participatingMembers = 3;
+
+    for (uint i = 0; i < totalMembers; i++) {
+      memberBalances[i] = lerc20Token.balanceOf(committeeMembers[i]);
+    }
+
+    uint256 reportId = generateReport(address(lerc20Token), maliciousActor, reporter);
+    
+    solveReport(reportId, participatingMembers, true, true, true);
+
+    for (uint i = 0; i < totalMembers; i++) {
+      evm.startPrank(committeeMembers[i]);
+      if (i < participatingMembers) {
+        lssGovernance.claimCommitteeReward(reportId);
+        uint256 newBalance =  memberBalances[i] + ((reportedAmount * committeeReward) / 1e2) / participatingMembers;
+        assertEq(lerc20Token.balanceOf(committeeMembers[i]), newBalance);  
+      } else {
+        evm.expectRevert("LSS: Did not vote on report");
+        lssGovernance.claimCommitteeReward(reportId);
+      }
+      evm.stopPrank();
+    }
+  }
 }
