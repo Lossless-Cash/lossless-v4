291fd332768ad0d2e2e68ee65c6a8a7fb923cd2e
Ignacio Freire <nachofreire@gmail.com>
2022-04-28 11:04:32 -0300

    refactor: move falttened to a separate folder

---
+++ contracts/LERC20Burnable.sol
@@ -0,0 +1,46 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "./utils/first-version/LERC20.sol";
+import "@openzeppelin/contracts/utils/Context.sol";
+
+contract LERC20Burnable is Context, LERC20 {
+
+    constructor(
+    uint256 totalSupply_, 
+    string memory name_, 
+    string memory symbol_, 
+    address admin_, 
+    address recoveryAdmin_, 
+    uint256 timelockPeriod_, 
+    address lossless_
+    ) LERC20(
+    totalSupply_, 
+    name_, 
+    symbol_, 
+    admin_, 
+    recoveryAdmin_, 
+    timelockPeriod_, 
+    lossless_
+    ) {}
+
+    modifier lssBurn(address account, uint256 amount) {
+        if (isLosslessOn) {
+            lossless.beforeBurn(account, amount);
+        } 
+        _;
+    }
+
+    function burn(uint256 amount) public virtual lssBurn(_msgSender(), amount) {
+        _burn(_msgSender(), amount);
+    }
+
+    function burnFrom(address account, uint256 amount) public virtual lssBurn(account, amount) {
+        uint256 currentAllowance = allowance(account, _msgSender());
+        require(currentAllowance >= amount, "ERC20: burn amount exceeds allowance");
+        unchecked {
+            _approve(account, _msgSender(), currentAllowance - amount);
+        }
+        _burn(account, amount);
+    }
+}
