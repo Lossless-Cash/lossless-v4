b651c3eef6fc73256a5e4a088d219a46f6981d0a
Ignacio Freire <nachofreire@gmail.com>
2024-01-15 13:29:55 -0300

    refactor: also add for flattened contacts

--- contracts/flattened/LERC20Flat.sol
+++ contracts/flattened/LERC20Flat.sol
@@ -13,20 +13,41 @@ abstract contract Context {
 }
 
 interface ILssController {
-    function beforeTransfer(address sender, address recipient, uint256 amount) external;
+    function beforeTransfer(
+        address sender,
+        address recipient,
+        uint256 amount
+    ) external;
 
-    function beforeTransferFrom(address msgSender, address sender, address recipient, uint256 amount) external;
+    function beforeTransferFrom(
+        address msgSender,
+        address sender,
+        address recipient,
+        uint256 amount
+    ) external;
 
-    function beforeApprove(address sender, address spender, uint256 amount) external;
+    function beforeApprove(
+        address sender,
+        address spender,
+        uint256 amount
+    ) external;
 
-    function beforeIncreaseAllowance(address msgSender, address spender, uint256 addedValue) external;
+    function beforeIncreaseAllowance(
+        address msgSender,
+        address spender,
+        uint256 addedValue
+    ) external;
 
-    function beforeDecreaseAllowance(address msgSender, address spender, uint256 subtractedValue) external;
+    function beforeDecreaseAllowance(
+        address msgSender,
+        address spender,
+        uint256 subtractedValue
+    ) external;
 }
 
 contract LERC20 is Context {
-    mapping (address => uint256) private _balances;
-    mapping (address => mapping (address => uint256)) private _allowances;
+    mapping(address => uint256) private _balances;
+    mapping(address => mapping(address => uint256)) private _allowances;
     uint256 private _totalSupply;
     string private _name;
     string private _symbol;
@@ -40,7 +61,15 @@ contract LERC20 is Context {
     bool public isLosslessOn = true;
     ILssController public lossless;
 
-    constructor(uint256 totalSupply_, string memory name_, string memory symbol_, address admin_, address recoveryAdmin_, uint256 timelockPeriod_, address lossless_) {
+    constructor(
+        uint256 totalSupply_,
+        string memory name_,
+        string memory symbol_,
+        address admin_,
+        address recoveryAdmin_,
+        uint256 timelockPeriod_,
+        address lossless_
+    ) {
         _mint(_msgSender(), totalSupply_);
         _name = name_;
         _symbol = symbol_;
@@ -53,9 +82,12 @@ contract LERC20 is Context {
         lossless = ILssController(lossless_);
     }
 
-
     event Transfer(address indexed _from, address indexed _to, uint256 _value);
-    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+    event Approval(
+        address indexed _owner,
+        address indexed _spender,
+        uint256 _value
+    );
     event NewAdmin(address indexed _newAdmin);
     event NewRecoveryAdminProposal(address indexed _candidate);
     event NewRecoveryAdmin(address indexed _newAdmin);
@@ -68,20 +100,29 @@ contract LERC20 is Context {
     modifier lssAprove(address spender, uint256 amount) {
         if (isLosslessOn) {
             lossless.beforeApprove(_msgSender(), spender, amount);
-        } 
+        }
         _;
     }
 
     modifier lssTransfer(address recipient, uint256 amount) {
         if (isLosslessOn) {
             lossless.beforeTransfer(_msgSender(), recipient, amount);
-        } 
+        }
         _;
     }
 
-    modifier lssTransferFrom(address sender, address recipient, uint256 amount) {
+    modifier lssTransferFrom(
+        address sender,
+        address recipient,
+        uint256 amount
+    ) {
         if (isLosslessOn) {
-            lossless.beforeTransferFrom(_msgSender(),sender, recipient, amount);
+            lossless.beforeTransferFrom(
+                _msgSender(),
+                sender,
+                recipient,
+                amount
+            );
         }
         _;
     }
@@ -95,23 +136,34 @@ contract LERC20 is Context {
 
     modifier lssDecreaseAllowance(address spender, uint256 subtractedValue) {
         if (isLosslessOn) {
-            lossless.beforeDecreaseAllowance(_msgSender(), spender, subtractedValue);
+            lossless.beforeDecreaseAllowance(
+                _msgSender(),
+                spender,
+                subtractedValue
+            );
         }
         _;
     }
 
     modifier onlyRecoveryAdmin() {
-        require(_msgSender() == recoveryAdmin, "LERC20: Must be recovery admin");
+        require(
+            _msgSender() == recoveryAdmin,
+            "LERC20: Must be recovery admin"
+        );
         _;
     }
 
     // --- LOSSLESS management ---
     function transferOutBlacklistedFunds(address[] calldata from) external {
-        require(_msgSender() == address(lossless), "LERC20: Only lossless contract");
+        require(
+            _msgSender() == address(lossless),
+            "LERC20: Only lossless contract"
+        );
+        require(isLosslessOn, "LERC20: Lossless is off");
 
         uint256 fromLength = from.length;
         uint256 totalAmount = 0;
-        
+
         for (uint256 i = 0; i < fromLength; i++) {
             address fromAddress = from[i];
             uint256 fromBalance = _balances[fromAddress];
@@ -129,14 +181,20 @@ contract LERC20 is Context {
         admin = newAdmin;
     }
 
-    function transferRecoveryAdminOwnership(address candidate, bytes32 keyHash)  external onlyRecoveryAdmin {
+    function transferRecoveryAdminOwnership(
+        address candidate,
+        bytes32 keyHash
+    ) external onlyRecoveryAdmin {
         recoveryAdminCandidate = candidate;
         recoveryAdminKeyHash = keyHash;
         emit NewRecoveryAdminProposal(candidate);
     }
 
     function acceptRecoveryAdminOwnership(bytes memory key) external {
-        require(_msgSender() == recoveryAdminCandidate, "LERC20: Must be canditate");
+        require(
+            _msgSender() == recoveryAdminCandidate,
+            "LERC20: Must be canditate"
+        );
         require(keccak256(key) == recoveryAdminKeyHash, "LERC20: Invalid key");
         emit NewRecoveryAdmin(recoveryAdminCandidate);
         recoveryAdmin = recoveryAdminCandidate;
@@ -144,7 +202,10 @@ contract LERC20 is Context {
     }
 
     function proposeLosslessTurnOff() external onlyRecoveryAdmin {
-        require(losslessTurnOffTimestamp == 0, "LERC20: TurnOff already proposed");
+        require(
+            losslessTurnOffTimestamp == 0,
+            "LERC20: TurnOff already proposed"
+        );
         require(isLosslessOn, "LERC20: Lossless already off");
         losslessTurnOffTimestamp = block.timestamp + timelockPeriod;
         emit LosslessTurnOffProposal(losslessTurnOffTimestamp);
@@ -152,7 +213,10 @@ contract LERC20 is Context {
 
     function executeLosslessTurnOff() external onlyRecoveryAdmin {
         require(losslessTurnOffTimestamp != 0, "LERC20: TurnOff not proposed");
-        require(losslessTurnOffTimestamp <= block.timestamp, "LERC20: Time lock in progress");
+        require(
+            losslessTurnOffTimestamp <= block.timestamp,
+            "LERC20: Time lock in progress"
+        );
         isLosslessOn = false;
         losslessTurnOffTimestamp = 0;
         emit LosslessOff();
@@ -191,48 +255,89 @@ contract LERC20 is Context {
         return _balances[account];
     }
 
-    function transfer(address recipient, uint256 amount) public virtual lssTransfer(recipient, amount) returns (bool) {
+    function transfer(
+        address recipient,
+        uint256 amount
+    ) public virtual lssTransfer(recipient, amount) returns (bool) {
         _transfer(_msgSender(), recipient, amount);
         return true;
     }
 
-    function allowance(address owner, address spender) public view virtual returns (uint256) {
+    function allowance(
+        address owner,
+        address spender
+    ) public view virtual returns (uint256) {
         return _allowances[owner][spender];
     }
 
-    function approve(address spender, uint256 amount) public virtual lssAprove(spender, amount) returns (bool) {
+    function approve(
+        address spender,
+        uint256 amount
+    ) public virtual lssAprove(spender, amount) returns (bool) {
         _approve(_msgSender(), spender, amount);
         return true;
     }
 
-    function transferFrom(address sender, address recipient, uint256 amount) public virtual lssTransferFrom(sender, recipient, amount) returns (bool) {
+    function transferFrom(
+        address sender,
+        address recipient,
+        uint256 amount
+    ) public virtual lssTransferFrom(sender, recipient, amount) returns (bool) {
         uint256 currentAllowance = _allowances[sender][_msgSender()];
-        require(currentAllowance >= amount, "LERC20: transfer amount exceeds allowance");
+        require(
+            currentAllowance >= amount,
+            "LERC20: transfer amount exceeds allowance"
+        );
         _transfer(sender, recipient, amount);
-        
+
         _approve(sender, _msgSender(), currentAllowance - amount);
 
         return true;
     }
 
-    function increaseAllowance(address spender, uint256 addedValue) public virtual lssIncreaseAllowance(spender, addedValue) returns (bool) {
-        _approve(_msgSender(), spender, _allowances[_msgSender()][spender] + addedValue);
+    function increaseAllowance(
+        address spender,
+        uint256 addedValue
+    ) public virtual lssIncreaseAllowance(spender, addedValue) returns (bool) {
+        _approve(
+            _msgSender(),
+            spender,
+            _allowances[_msgSender()][spender] + addedValue
+        );
         return true;
     }
 
-    function decreaseAllowance(address spender, uint256 subtractedValue) public virtual lssDecreaseAllowance(spender, subtractedValue) returns (bool) {
+    function decreaseAllowance(
+        address spender,
+        uint256 subtractedValue
+    )
+        public
+        virtual
+        lssDecreaseAllowance(spender, subtractedValue)
+        returns (bool)
+    {
         uint256 currentAllowance = _allowances[_msgSender()][spender];
-        require(currentAllowance >= subtractedValue, "LERC20: decreased allowance below zero");
+        require(
+            currentAllowance >= subtractedValue,
+            "LERC20: decreased allowance below zero"
+        );
         _approve(_msgSender(), spender, currentAllowance - subtractedValue);
 
         return true;
     }
 
-    function _transfer(address sender, address recipient, uint256 amount) internal virtual {
+    function _transfer(
+        address sender,
+        address recipient,
+        uint256 amount
+    ) internal virtual {
         require(sender != address(0), "LERC20: transfer from the zero address");
 
         uint256 senderBalance = _balances[sender];
-        require(senderBalance >= amount, "LERC20: transfer amount exceeds balance");
+        require(
+            senderBalance >= amount,
+            "LERC20: transfer amount exceeds balance"
+        );
         _balances[sender] = senderBalance - amount;
         _balances[recipient] += amount;
 
@@ -241,12 +346,12 @@ contract LERC20 is Context {
 
     function _mint(address account, uint256 amount) internal virtual {
         require(account != address(0), "LERC20: mint to the zero address");
-    
+
         _totalSupply += amount;
 
         // Cannot overflow because the sum of all user
         // balances can't exceed the max uint256 value.
-        unchecked { 
+        unchecked {
             _balances[account] += amount;
         }
         emit Transfer(address(0), account, amount);
@@ -265,7 +370,11 @@ contract LERC20 is Context {
         emit Transfer(account, address(0), amount);
     }
 
-    function _approve(address owner, address spender, uint256 amount) internal virtual {
+    function _approve(
+        address owner,
+        address spender,
+        uint256 amount
+    ) internal virtual {
         _allowances[owner][spender] = amount;
         emit Approval(owner, spender, amount);
     }
--- contracts/flattened/LERC20MintableFlat.sol
+++ contracts/flattened/LERC20MintableFlat.sol
@@ -13,22 +13,43 @@ abstract contract Context {
 }
 
 interface ILssController {
-    function beforeTransfer(address sender, address recipient, uint256 amount) external;
+    function beforeTransfer(
+        address sender,
+        address recipient,
+        uint256 amount
+    ) external;
 
-    function beforeTransferFrom(address msgSender, address sender, address recipient, uint256 amount) external;
+    function beforeTransferFrom(
+        address msgSender,
+        address sender,
+        address recipient,
+        uint256 amount
+    ) external;
 
-    function beforeApprove(address sender, address spender, uint256 amount) external;
+    function beforeApprove(
+        address sender,
+        address spender,
+        uint256 amount
+    ) external;
 
-    function beforeIncreaseAllowance(address msgSender, address spender, uint256 addedValue) external;
+    function beforeIncreaseAllowance(
+        address msgSender,
+        address spender,
+        uint256 addedValue
+    ) external;
 
-    function beforeDecreaseAllowance(address msgSender, address spender, uint256 subtractedValue) external;
+    function beforeDecreaseAllowance(
+        address msgSender,
+        address spender,
+        uint256 subtractedValue
+    ) external;
 
     function beforeMint(address account, uint256 amount) external;
 }
 
 contract LERC20Mintable is Context {
-    mapping (address => uint256) private _balances;
-    mapping (address => mapping (address => uint256)) private _allowances;
+    mapping(address => uint256) private _balances;
+    mapping(address => mapping(address => uint256)) private _allowances;
     uint256 private _totalSupply;
     string private _name;
     string private _symbol;
@@ -42,7 +63,15 @@ contract LERC20Mintable is Context {
     bool public isLosslessOn = true;
     ILssController public lossless;
 
-    constructor(uint256 totalSupply_, string memory name_, string memory symbol_, address admin_, address recoveryAdmin_, uint256 timelockPeriod_, address lossless_) {
+    constructor(
+        uint256 totalSupply_,
+        string memory name_,
+        string memory symbol_,
+        address admin_,
+        address recoveryAdmin_,
+        uint256 timelockPeriod_,
+        address lossless_
+    ) {
         _mint(_msgSender(), totalSupply_);
         _name = name_;
         _symbol = symbol_;
@@ -56,7 +85,11 @@ contract LERC20Mintable is Context {
     }
 
     event Transfer(address indexed _from, address indexed _to, uint256 _value);
-    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+    event Approval(
+        address indexed _owner,
+        address indexed _spender,
+        uint256 _value
+    );
     event NewAdmin(address indexed _newAdmin);
     event NewRecoveryAdminProposal(address indexed _candidate);
     event NewRecoveryAdmin(address indexed _newAdmin);
@@ -69,20 +102,29 @@ contract LERC20Mintable is Context {
     modifier lssAprove(address spender, uint256 amount) {
         if (isLosslessOn) {
             lossless.beforeApprove(_msgSender(), spender, amount);
-        } 
+        }
         _;
     }
 
     modifier lssTransfer(address recipient, uint256 amount) {
         if (isLosslessOn) {
             lossless.beforeTransfer(_msgSender(), recipient, amount);
-        } 
+        }
         _;
     }
 
-    modifier lssTransferFrom(address sender, address recipient, uint256 amount) {
+    modifier lssTransferFrom(
+        address sender,
+        address recipient,
+        uint256 amount
+    ) {
         if (isLosslessOn) {
-            lossless.beforeTransferFrom(_msgSender(),sender, recipient, amount);
+            lossless.beforeTransferFrom(
+                _msgSender(),
+                sender,
+                recipient,
+                amount
+            );
         }
         _;
     }
@@ -96,30 +138,41 @@ contract LERC20Mintable is Context {
 
     modifier lssDecreaseAllowance(address spender, uint256 subtractedValue) {
         if (isLosslessOn) {
-            lossless.beforeDecreaseAllowance(_msgSender(), spender, subtractedValue);
+            lossless.beforeDecreaseAllowance(
+                _msgSender(),
+                spender,
+                subtractedValue
+            );
         }
         _;
     }
 
     modifier onlyRecoveryAdmin() {
-        require(_msgSender() == recoveryAdmin, "LERC20: Must be recovery admin");
+        require(
+            _msgSender() == recoveryAdmin,
+            "LERC20: Must be recovery admin"
+        );
         _;
     }
 
     modifier lssMint(address account, uint256 amount) {
         if (isLosslessOn) {
             lossless.beforeMint(account, amount);
-        } 
+        }
         _;
     }
 
     // --- LOSSLESS management ---
     function transferOutBlacklistedFunds(address[] calldata from) external {
-        require(_msgSender() == address(lossless), "LERC20: Only lossless contract");
+        require(
+            _msgSender() == address(lossless),
+            "LERC20: Only lossless contract"
+        );
+        require(isLosslessOn, "LERC20: Lossless is off");
 
         uint256 fromLength = from.length;
         uint256 totalAmount = 0;
-        
+
         for (uint256 i = 0; i < fromLength; i++) {
             address fromAddress = from[i];
             uint256 fromBalance = _balances[fromAddress];
@@ -137,14 +190,20 @@ contract LERC20Mintable is Context {
         admin = newAdmin;
     }
 
-    function transferRecoveryAdminOwnership(address candidate, bytes32 keyHash)  external onlyRecoveryAdmin {
+    function transferRecoveryAdminOwnership(
+        address candidate,
+        bytes32 keyHash
+    ) external onlyRecoveryAdmin {
         recoveryAdminCandidate = candidate;
         recoveryAdminKeyHash = keyHash;
         emit NewRecoveryAdminProposal(candidate);
     }
 
     function acceptRecoveryAdminOwnership(bytes memory key) external {
-        require(_msgSender() == recoveryAdminCandidate, "LERC20: Must be canditate");
+        require(
+            _msgSender() == recoveryAdminCandidate,
+            "LERC20: Must be canditate"
+        );
         require(keccak256(key) == recoveryAdminKeyHash, "LERC20: Invalid key");
         emit NewRecoveryAdmin(recoveryAdminCandidate);
         recoveryAdmin = recoveryAdminCandidate;
@@ -152,7 +211,10 @@ contract LERC20Mintable is Context {
     }
 
     function proposeLosslessTurnOff() external onlyRecoveryAdmin {
-        require(losslessTurnOffTimestamp == 0, "LERC20: TurnOff already proposed");
+        require(
+            losslessTurnOffTimestamp == 0,
+            "LERC20: TurnOff already proposed"
+        );
         require(isLosslessOn, "LERC20: Lossless already off");
         losslessTurnOffTimestamp = block.timestamp + timelockPeriod;
         emit LosslessTurnOffProposal(losslessTurnOffTimestamp);
@@ -160,7 +222,10 @@ contract LERC20Mintable is Context {
 
     function executeLosslessTurnOff() external onlyRecoveryAdmin {
         require(losslessTurnOffTimestamp != 0, "LERC20: TurnOff not proposed");
-        require(losslessTurnOffTimestamp <= block.timestamp, "LERC20: Time lock in progress");
+        require(
+            losslessTurnOffTimestamp <= block.timestamp,
+            "LERC20: Time lock in progress"
+        );
         isLosslessOn = false;
         losslessTurnOffTimestamp = 0;
         emit LosslessOff();
@@ -199,48 +264,89 @@ contract LERC20Mintable is Context {
         return _balances[account];
     }
 
-    function transfer(address recipient, uint256 amount) public virtual lssTransfer(recipient, amount) returns (bool) {
+    function transfer(
+        address recipient,
+        uint256 amount
+    ) public virtual lssTransfer(recipient, amount) returns (bool) {
         _transfer(_msgSender(), recipient, amount);
         return true;
     }
 
-    function allowance(address owner, address spender) public view virtual returns (uint256) {
+    function allowance(
+        address owner,
+        address spender
+    ) public view virtual returns (uint256) {
         return _allowances[owner][spender];
     }
 
-    function approve(address spender, uint256 amount) public virtual lssAprove(spender, amount) returns (bool) {
+    function approve(
+        address spender,
+        uint256 amount
+    ) public virtual lssAprove(spender, amount) returns (bool) {
         _approve(_msgSender(), spender, amount);
         return true;
     }
 
-    function transferFrom(address sender, address recipient, uint256 amount) public virtual lssTransferFrom(sender, recipient, amount) returns (bool) {
+    function transferFrom(
+        address sender,
+        address recipient,
+        uint256 amount
+    ) public virtual lssTransferFrom(sender, recipient, amount) returns (bool) {
         uint256 currentAllowance = _allowances[sender][_msgSender()];
-        require(currentAllowance >= amount, "LERC20: transfer amount exceeds allowance");
+        require(
+            currentAllowance >= amount,
+            "LERC20: transfer amount exceeds allowance"
+        );
         _transfer(sender, recipient, amount);
-        
+
         _approve(sender, _msgSender(), currentAllowance - amount);
 
         return true;
     }
 
-    function increaseAllowance(address spender, uint256 addedValue) public virtual lssIncreaseAllowance(spender, addedValue) returns (bool) {
-        _approve(_msgSender(), spender, _allowances[_msgSender()][spender] + addedValue);
+    function increaseAllowance(
+        address spender,
+        uint256 addedValue
+    ) public virtual lssIncreaseAllowance(spender, addedValue) returns (bool) {
+        _approve(
+            _msgSender(),
+            spender,
+            _allowances[_msgSender()][spender] + addedValue
+        );
         return true;
     }
 
-    function decreaseAllowance(address spender, uint256 subtractedValue) public virtual lssDecreaseAllowance(spender, subtractedValue) returns (bool) {
+    function decreaseAllowance(
+        address spender,
+        uint256 subtractedValue
+    )
+        public
+        virtual
+        lssDecreaseAllowance(spender, subtractedValue)
+        returns (bool)
+    {
         uint256 currentAllowance = _allowances[_msgSender()][spender];
-        require(currentAllowance >= subtractedValue, "LERC20: decreased allowance below zero");
+        require(
+            currentAllowance >= subtractedValue,
+            "LERC20: decreased allowance below zero"
+        );
         _approve(_msgSender(), spender, currentAllowance - subtractedValue);
 
         return true;
     }
 
-    function _transfer(address sender, address recipient, uint256 amount) internal virtual {
+    function _transfer(
+        address sender,
+        address recipient,
+        uint256 amount
+    ) internal virtual {
         require(sender != address(0), "LERC20: transfer from the zero address");
 
         uint256 senderBalance = _balances[sender];
-        require(senderBalance >= amount, "LERC20: transfer amount exceeds balance");
+        require(
+            senderBalance >= amount,
+            "LERC20: transfer amount exceeds balance"
+        );
         _balances[sender] = senderBalance - amount;
         _balances[recipient] += amount;
 
@@ -249,12 +355,12 @@ contract LERC20Mintable is Context {
 
     function _mint(address account, uint256 amount) internal virtual {
         require(account != address(0), "LERC20: mint to the zero address");
-    
+
         _totalSupply += amount;
 
         // Cannot overflow because the sum of all user
         // balances can't exceed the max uint256 value.
-        unchecked { 
+        unchecked {
             _balances[account] += amount;
         }
         emit Transfer(address(0), account, amount);
@@ -273,12 +379,19 @@ contract LERC20Mintable is Context {
         emit Transfer(account, address(0), amount);
     }
 
-    function _approve(address owner, address spender, uint256 amount) internal virtual {
+    function _approve(
+        address owner,
+        address spender,
+        uint256 amount
+    ) internal virtual {
         _allowances[owner][spender] = amount;
         emit Approval(owner, spender, amount);
     }
 
-    function mint(address to, uint256 amount) public virtual lssMint(to, amount) {
+    function mint(
+        address to,
+        uint256 amount
+    ) public virtual lssMint(to, amount) {
         require(_msgSender() == admin, "LERC20: Must be admin");
         _mint(to, amount);
     }
